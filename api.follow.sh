5e4ee13c6e86688b5c8d5a6591be726d08a473bf
catonrug <aigars.kadikis@gmail.com>
2025-11-17 19:48:50 +0200

    next

--- api.follow.sh
+++ api.follow.sh
@@ -14,6 +14,7 @@ echo ${NR}
 # order of follow ups
 echo "
 user.login
+proxy.get
 " | grep -v "^$" | while IFS= read -r STEP
 do {
 
@@ -23,16 +24,33 @@ do {
 
 VER=$(echo "${MINVER}" | grep -Eo "[0-9]+")
 
-[ "${NR}" -lt "${VER}" ] && BODY=${MINVER} && echo "${MINVER}" && cat "${BODY}" > /tmp/body.json && break
+[ "${NR}" -lt "${VER}" ] && BODY=${MINVER} && echo "${MINVER}" > /tmp/path.cat && cat "${BODY}" > /tmp/body.json && break
 
 } done
 
 # store session token
-curl --silent \
+echo 'user.login' | grep "${STEP}" > /dev/null
+[ "$?" -eq "0" ] && curl --silent \
 --request POST \
 --header 'Content-Type: application/json-rpc' \
 --data "$(jq . /tmp/body.json -c)" \
 http://127.0.0.1:${LINE}/api_jsonrpc.php | grep -Eo "([0-9a-f]{32,32})" | tee /tmp/.zabbix.api.${NR}
+
+echo 'proxy.get' | grep "${STEP}" > /dev/null
+[ "$?" -eq "0" ] && \
+if [ "${LINE}" -lt "8063" ]; then
+
+curl --silent \
+--request POST \
+--header 'Content-Type: application/json-rpc' \
+--data "$(jq . $(cat /tmp/path.cat) -c | sed "s|TOKEN|$(cat /tmp/.zabbix.api.${NR})|")" \
+http://127.0.0.1:${LINE}/api_jsonrpc.php
+
+else
+
+echo asdf
+
+fi
 
 } done
 
