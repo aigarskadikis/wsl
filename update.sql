862db56d3cc471263070256168b1f6d49b8e4570
catonrug <aigars.kadikis@gmail.com>
2025-11-18 18:58:45 +0200

    next

--- update.sql
+++ update.sql
@@ -26,3 +26,10 @@ SELECT type FROM users WHERE username='Admin'
 
 --Zabbix 6.0
 UPDATE users SET rows_per_page=2000 WHERE rows_per_page=50;
+
+--Zabbix 8.0. enable automatic inventory
+UPDATE settings SET value_int=1 WHERE name='default_inventory_mode' AND value_int='-1';
+
+--Zabbix 8.0. do not show server down errors
+UPDATE settings SET value_int=0 WHERE name='server_check_interval' AND type=2 AND value_int=10;
+
